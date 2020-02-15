478d55ff2a8d719fb5724ab73aa38ec0dc1f76e0
Shynixn <shynixn@gmail.com>
2020-02-15 13:57:03 +0100

    Added breadcrumbs and book edit.

--- src/app/bookdetails/bookdetails.component.ts
+++ src/app/bookdetails/bookdetails.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as M from 'materialize-css';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-bookdetails',
@@ -8,6 +9,18 @@ import * as M from 'materialize-css';
 })
 export class BookdetailsComponent implements OnInit {
   /**
+   * Link way.
+   */
+  way: Array<Breadcrumb> = [
+    {
+      displayName: "Collection",
+      link: "/collection"
+    },
+    {
+      displayName: "Demo Book",
+      link: "/collection/1"
+    }];
+  /**
    * Is this component editable?
    */
   editable: boolean;
@@ -15,15 +28,20 @@ export class BookdetailsComponent implements OnInit {
    * Editing book.
    */
   book: Book;
+  /**
+   * Edit Form.
+   */
+  editForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.book = {
-      name : "Demo Book",
-      author : "Max Mustermann",
-      description : "This is not a very long description.",
-      isbn : "978-41231243",
-      releaseDate : new Date()
-    }
+      name: "Demo Book",
+      author: "Max Mustermann",
+      description: "This is not a very long description.",
+      isbn: "978-41231243",
+      releaseDate: new Date()
+    };
+    this.convertBookToEditForm()
   }
 
   ngOnInit() {
@@ -37,11 +55,37 @@ export class BookdetailsComponent implements OnInit {
    * Toggles the editable state.
    */
   toggleEditState() {
-    this.editable = !this.editable
+    this.editable = !this.editable;
+    if (this.editable) {
+      this.convertBookToEditForm();
+    }
   }
 
-  save() {
+  ngAfterViewChecked() {
+    M.updateTextFields();
+  }
+
+  /**
+   * Executes a book save.
+   */
+  onSave(submitData) {
+
     this.toggleEditState();
-    M.toast({html: 'This book was successfully saved'})
+    M.toast({html: 'This book was successfully saved'});
+
+    this.book.isbn = submitData.isbn;
+    this.book.author = submitData.author;
+    this.book.releaseDate = new Date(submitData.releaseDate);
+  }
+
+  /**
+   * Converts the book to an edit form.
+   */
+  private convertBookToEditForm() {
+    this.editForm = this.formBuilder.group({
+      isbn: this.book.isbn,
+      author: this.book.author,
+      releaseDate: this.book.releaseDate.toDateString()
+    });
   }
 }
